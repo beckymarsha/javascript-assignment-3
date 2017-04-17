2f87e1a12e388035b9e0629f7dc5b9518c9f9c18
Becky Marshall <becky.marshall@id.iit.edu>
2017-04-17 12:46:04 -0500

    I changed it all to the “while” loop, and fixed the div element, but I’m still getting 5 ray-1s

--- script.js
+++ script.js
@@ -12,37 +12,29 @@ var sun = document.createElement("div");
 sun.className = "sun";
 view.appendChild(sun);
 
-function makeRay() {
 
-var ray1 = document.createElement("ray-1");
+
+var x = 0;
+while (x < 6) {
+var ray1 = document.createElement("div");
 ray1.className = "ray-1";
 sun.appendChild(ray1);
 
-var ray2 = document.createElement("ray-2");
+var ray2 = document.createElement("div");
 ray2.className = "ray-2";
 sun.appendChild(ray2);
 
-var ray3 = document.createElement("ray-3");
+var ray3 = document.createElement("div");
 ray3.className = "ray-3";
 sun.appendChild(ray3);
 
-var ray4 = document.createElement("ray-4");
+var ray4 = document.createElement("div");
 ray4.className = "ray-4";
 sun.appendChild(ray4);
 
-var ray5 = document.createElement("ray-5");
+var ray5 = document.createElement("div");
 ray5.className = "ray-5";
 sun.appendChild(ray5);
 
-}
-
-var x = 0;
-while (x < 2) {
-    makeRay();
-    sun.appendChild(ray1);
-    sun.appendChild(ray2);
-    sun.appendChild(ray3);
-    sun.appendChild(ray4);
-    sun.appendChild(ray5);
-    x++;
+x++;
 }
