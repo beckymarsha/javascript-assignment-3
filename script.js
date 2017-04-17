065fc4607ab06a3879d1e96e5043a7450841dcb0
Becky Marshall <becky.marshall@id.iit.edu>
2017-04-17 12:21:23 -0500

    created divs

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 // Do your work in this file.
 
+var view = document.createElement("div");
+view.className = "view";
+//view.innerText = "My first car was gray!";
+document.body.appendChild(view);
+
+var grass = document.createElement("div");
+grass.className = "grass";
+//view.innerText = "My first car was gray!";
+document.body.appendChild(grass);
+
+var sun = document.createElement("div");
+sun.className = "sun";
+//view.innerText = "My first car was gray!";
+document.body.appendChild(sun);
