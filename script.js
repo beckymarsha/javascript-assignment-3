01d7c7bea15d03bf5dc0cef4cfdae78ec0b9a920
Becky Marshall <becky.marshall@id.iit.edu>
2017-04-17 12:44:10 -0500

    I’ve appended 5 ray-1’s to the sun, but I want ray-1 through ray-5…

--- script.js
+++ script.js
@@ -2,15 +2,47 @@
 
 var view = document.createElement("div");
 view.className = "view";
-//view.innerText = "My first car was gray!";
 document.body.appendChild(view);
 
 var grass = document.createElement("div");
 grass.className = "grass";
-//view.innerText = "My first car was gray!";
 view.appendChild(grass);
 
 var sun = document.createElement("div");
 sun.className = "sun";
-//view.innerText = "My first car was gray!";
 view.appendChild(sun);
+
+function makeRay() {
+
+var ray1 = document.createElement("ray-1");
+ray1.className = "ray-1";
+sun.appendChild(ray1);
+
+var ray2 = document.createElement("ray-2");
+ray2.className = "ray-2";
+sun.appendChild(ray2);
+
+var ray3 = document.createElement("ray-3");
+ray3.className = "ray-3";
+sun.appendChild(ray3);
+
+var ray4 = document.createElement("ray-4");
+ray4.className = "ray-4";
+sun.appendChild(ray4);
+
+var ray5 = document.createElement("ray-5");
+ray5.className = "ray-5";
+sun.appendChild(ray5);
+
+}
+
+var x = 0;
+while (x < 2) {
+    makeRay();
+    sun.appendChild(ray1);
+    sun.appendChild(ray2);
+    sun.appendChild(ray3);
+    sun.appendChild(ray4);
+    sun.appendChild(ray5);
+    x++;
+}
