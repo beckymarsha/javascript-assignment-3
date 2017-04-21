e68b794bf1765275408917337428a48b1813d381
Becky Marshall <becky.marshall@id.iit.edu>
2017-04-20 19:11:54 -0500

    Realized the ray error was because of Safari.  (the transform rotate doesn’t work in safari the same way).  Started working in Chrome. Added mountain and mountaintop

--- script.js
+++ script.js
@@ -12,6 +12,37 @@ var sun = document.createElement("div");
 sun.className = "sun";
 view.appendChild(sun);
 
+var ray1 = document.createElement("div");
+ray1.className = "ray-1";
+var ray2 = document.createElement("div");
+ray2.className = "ray-2";
+var ray3 = document.createElement("div");
+ray3.className = "ray-3";
+var ray4 = document.createElement("div");
+ray4.className = "ray-4";
+var ray5 = document.createElement("div");
+ray5.className = "ray-5";
+
+var x = 0;
+while (x < 2) {
+    sun.appendChild(ray1);
+    sun.appendChild(ray2);
+    sun.appendChild(ray3);
+    sun.appendChild(ray4);
+    sun.appendChild(ray5);
+    x++;
+}   
+
+var mountain = document.createElement("div");
+mountain.className = "mountain";
+view.appendChild(mountain);
+
+var mountaintop = document.createElement("div");
+mountaintop.className = "mountain-top";
+mountain.appendChild(mountaintop);
+
+
+
 // var ray1 = document.createElement("div");
 // ray1.className = "ray-1";
 // sun.appendChild(ray1);
@@ -34,16 +65,3 @@ view.appendChild(sun);
 
 
 
-// var x = 0;
-// while (x < 2) {
-//     sun.appendChild(ray1);
-//     sun.appendChild(ray2);
-//     sun.appendChild(ray3);
-//     sun.appendChild(ray4);
-//     sun.appendChild(ray5);
-//     x++;
-// }              var x = 0;
-// while (x < 6) {
-//     sun.appendChild(ray1);
-//     x++;
-// }
