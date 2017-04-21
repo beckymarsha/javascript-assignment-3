a56a016388ec28676b9acb1ce93da4c5a4379fc3
Becky Marshall <becky.marshall@id.iit.edu>
2017-04-21 10:42:24 -0500

    correct form for ‘while’ loop

--- script.js
+++ script.js
@@ -12,25 +12,34 @@ var sun = document.createElement("div");
 sun.className = "sun";
 view.appendChild(sun);
 
-var ray1 = document.createElement("div");
-ray1.className = "ray-1";
-var ray2 = document.createElement("div");
-ray2.className = "ray-2";
-var ray3 = document.createElement("div");
-ray3.className = "ray-3";
-var ray4 = document.createElement("div");
-ray4.className = "ray-4";
-var ray5 = document.createElement("div");
-ray5.className = "ray-5";
+// var ray1 = document.createElement("div");
+// ray1.className = "ray-1";
+// var ray2 = document.createElement("div");
+// ray2.className = "ray-2";
+// var ray3 = document.createElement("div");
+// ray3.className = "ray-3";
+// var ray4 = document.createElement("div");
+// ray4.className = "ray-4";
+// var ray5 = document.createElement("div");
+// ray5.className = "ray-5";
 
-var x = 0;
-while (x < 2) {
-    sun.appendChild(ray1);
-    sun.appendChild(ray2);
-    sun.appendChild(ray3);
-    sun.appendChild(ray4);
-    sun.appendChild(ray5);
-    x++;
+// var x = 0;
+// while (x < 2) {
+//     sun.appendChild(ray1);
+//     sun.appendChild(ray2);
+//     sun.appendChild(ray3);
+//     sun.appendChild(ray4);
+//     sun.appendChild(ray5);
+//     x++;
+// }
+
+
+var y = 0
+while (y <= 5) {
+var ray = document.createElement("div");
+ray.className = "ray-" + y;
+sun.appendChild(ray);
+y++
 }
 
 var mountain = document.createElement("div");
